ed5f54b84ab62b638c37fa634ca4cb4d2c72b008
Getaneh Kunda <Getaneh.kudna.90@gmail.com>
2020-07-27 20:51:34 -0700

    testing test1 case

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from check_pwd import check_pwd
 class MyTestCase(unittest.TestCase):
     def test1(self):
 
-       input = '     '
+       input = '  1   '
        expected = False
        self.assertEqual(check_pwd(input), expected)
 
