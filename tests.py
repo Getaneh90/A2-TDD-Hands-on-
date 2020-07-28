da96cab92cc3da87cb7ec8f0bd35d2715cf18203
Getaneh Kunda <Getaneh.kudna.90@gmail.com>
2020-07-27 20:47:32 -0700

    first commit

---
+++ tests.py
@@ -0,0 +1,15 @@
+import random
+import unittest
+from check_pwd import check_pwd
+
+
+class MyTestCase(unittest.TestCase):
+    def test1(self):
+
+       input = '     '
+       expected = False
+       self.assertEqual(check_pwd(input), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
